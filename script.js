a687294dc7ad64e7e013823f7571213fa60c1e79
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-12-08 14:47:07 +0100

    Day_04

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ let width = 600;
 let height = 600;
 let ctx = canvas.getContext('2d');
 let blockSize = 30;
+let keys = [];
 
 canvas.width = width;
 canvas.height = height;
+
+let hero = new Image();
+hero.src = 'images/down.png';
+
+let player = {
+  x: 8,
+  y: 1,
+};
 
 let wall = new Image();
 wall.src = 'images/zed.png';
@@ -33,14 +42,63 @@ let board = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-for (let y = 0; y < board.length; y++) {
+function generateBoard() {
   for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < board[y].length; x++) {
       if (board[y][x] === 1) {
-        tx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
+        ctx.drawImage(wall, x * blockSize, y * blockSize, blockSize, blockSize);
       }
     }
   }
 }
 
-window.addEventListener('load', generateBoard);
+function draw() {
+  ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize)
+  generateBoard();
+  movement();
+  ctx.drawImage(
+    hero,
+    player.x * blockSize,
+    player.y * blockSize,
+    blockSize,
+    blockSize,
+  );
+}
+
+window.addEventListener('load', draw);
+
+document.body.addEventListener("keydown", function(e) {
+  keys[e.keyCode] = true
+  draw()
+})
+
+document.body.addEventListener("keyup", function(e) {
+  keys[e.keyCode] = false 
+  draw()
+})
+
+function movement() {
+  if (keys[39]) {
+      // šipka doprava
+      hero.src = "images/right.png"
+      player.x++
+  }
+
+  if (keys[37]) {
+      // šipka doleva
+      hero.src = "images/left.png"
+      player.x--
+  }
+
+  if (keys[38]) {
+      // šipka nahoru
+      hero.src = "images/up.png"
+      player.y--
+  }
+
+  if (keys[40]) {
+      // šipka dolů
+      hero.src = "images/down.png"
+      player.y++
+  }
+}
